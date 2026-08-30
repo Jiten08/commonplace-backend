efd474088b4e9a8bb82c21d716886a95fdefd644
Jiten <jitenksingh8@gmail.com>
2026-08-30 20:37:21 +0530

    added utils

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import dns from 'node:dns';
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Google DNS server
 
 dotenv.config({
   path: './env'
 })
 
 connectDB()
+  .then(()=>{
+    app.on("error", (error)=>{
+      console.errror(error)
+      process.exit(1)
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+  })
+  .catch((err)=>{
+    console.log('MONGO DB connection failed.', err)
+  })
